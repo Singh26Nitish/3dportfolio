3f16cb2a9e9bb125e8894436c7c60b0337dfd28c
Nitish Singh <nkushwaha791@gmail.com>
2025-01-10 23:27:23 +0530

    updated

--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -7,7 +7,7 @@ const Target = (props) => {
   const targetRef = useRef();
   const { scene } = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf')
 
-  useGLTF( () => {
+  useGSAP( () => {
     gsap.to(targetRef.current.position, {
       y: targetRef.current.position.y + 0.5,
       duration: 1.5,
